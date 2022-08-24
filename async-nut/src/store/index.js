a3442c23dad43c9b6b7d4bdf4cc9d80a57fab09d
heavenC <heavenofsky@qq.com>
2022-08-25 00:41:59 +0800

    redux-saga学习

--- async-nut/src/store/index.js
+++ async-nut/src/store/index.js
@@ -1,10 +1,17 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../aciotn/rootSaga";
 import { loginReducer } from "./loginReducer";
+
+
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
     combineReducers({ user: loginReducer }),
-    applyMiddleware(thunk)
+    applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
